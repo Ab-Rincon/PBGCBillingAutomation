f69f4182e9861444e43ae8938f727fb6dc466ea4
= <AbRincon@Deloitte.com>
2023-12-13 22:30:30 -0500

    Lint test_clean_data.py

--- tests/test_clean_data.py
+++ tests/test_clean_data.py
@@ -14,7 +14,7 @@ class TestCleanDataClass(unittest.TestCase):
     def test_calculate_total_time_difference(self):
         in_times = ['09:00', '13:00']
         out_times = ['12:00', '17:00']
-        expected_total_hours = 3+4  # Expected total hours
+        expected_total_hours = 3 + 4  # Expected total hours
         self.assertEqual(calculate_total_time_difference(in_times, out_times), expected_total_hours)
 
 
